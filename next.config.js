26351e46bb775b804474bfb2c97826f99aabf9fd
Ericufo <2252584@tongji.edu.cn>
2024-10-20 22:36:09 +0800

    3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  basePath: "/Ericufo.github.io", // 正确设置 basePath 为仓库名
-  assetPrefix: "/Ericufo.github.io", // 设置资源前缀
   output: "export", // 使用 'export' 进行静态导出
   trailingSlash: true, // 确保所有路径都以斜杠结尾
 };
